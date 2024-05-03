5a57643ecdf9d465cbda1c98a847a8d07fc6c708
RikoshetGames <ilyalew@mail.ru>
2024-05-03 23:13:58 +0300

    Add Users

---
+++ users/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.forms.widgets import SelectDateWidget
+
+from users.models import User
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2')
+
+
+class UserProfileForm(StyleFormMixin, UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'patronymic', 'birth_date', 'avatar', 'phone')
+        widgets = {
+            'birth_date': forms.TextInput(attrs={'placeholder': 'дд.мм.гггг'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['password'].widget = forms.HiddenInput()
