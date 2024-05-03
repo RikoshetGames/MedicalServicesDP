ad3298bbcf1144936281e4dc56031ffa62ff722c
RikoshetGames <ilyalew@mail.ru>
2024-05-03 23:21:44 +0300

    Add csu

---
+++ users/management/commands/csu.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user =User.objects.create(
+            email='rikoshet645@gmail.com',
+            first_name='Moder',
+            last_name='Moderov',
+            is_superuser=False,
+            is_staff=True,
+            is_active=True
+        )
+
+        user.set_password('645595715abcd')
+        user.save()
